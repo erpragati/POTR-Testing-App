a2a949ad61943b488d4719ac43a61ee4c52806b6
Marc Charbonneau <mcharbonneau@elctech.com>
2011-10-20 10:23:10 -0700

    Initial Commit

---
+++ POTR SDK Test App/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  POTR SDK Test App
+//
+//  Created by Marc Charbonneau on 10/20/11.
+//  Copyright (c) 2011 ELC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ POTR SDK Test App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  POTR SDK Test App
+//
+//  Created by Marc Charbonneau on 10/20/11.
+//  Copyright (c) 2011 ELC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
